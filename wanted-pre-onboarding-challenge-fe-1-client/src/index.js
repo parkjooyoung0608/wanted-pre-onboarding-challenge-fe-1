aa5c56a976eb7e426b41b53279dc9683a43fe4fc
parkjooyoung <mirkduddl@naver.com>
2022-08-03 21:58:57 +0900

    Add : Styled Component 설치 및 Login UI 작업 완료
    - Styled Componeet 설치 및 적용
    - GlobalStyle Reset css 적용
    - Login UI 작업 완료

--- wanted-pre-onboarding-challenge-fe-1-client/src/index.js
+++ wanted-pre-onboarding-challenge-fe-1-client/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
+import GlobalStyle from "./styles/global-styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <Router />
+      <GlobalStyle />
+    </React.StrictMode>
+  </>
 );
--- wanted-pre-onboarding-challenge-fe-1-client/src/pages/Login/Login.js
+++ wanted-pre-onboarding-challenge-fe-1-client/src/pages/Login/Login.js
@@ -1,7 +1,93 @@
 import React from "react";
+import styled from "styled-components";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <LoginPage>
+      <LoginContainer>
+        <LoginTitle>로그인</LoginTitle>
+        <form>
+          <LoginInputBox>
+            <Input type="email" placeholder="이메일을 입력해주세요"></Input>
+            <Input
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+            ></Input>
+            <LoignBtn>로그인</LoignBtn>
+          </LoginInputBox>
+        </form>
+      </LoginContainer>
+    </LoginPage>
+  );
 };
 
 export default Login;
+
+const LoginPage = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const LoginContainer = styled.div`
+  border: 1px solid lightgray;
+  text-align: center;
+  padding: 50px;
+  background-color: rgba(0, 0, 0, 0.1);
+`;
+
+const LoginTitle = styled.h1`
+  font-size: 30px;
+  margin-bottom: 30px;
+`;
+
+const LoginInputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  margin-bottom: 20px;
+  padding: 10px;
+  line-height: 40px;
+  border: 1px solid lightgray;
+  outline: none;
+  border-radius: 3px;
+
+  &:focus {
+    border: 1px solid gray;
+  }
+`;
+
+const LoignBtn = styled.button`
+  margin: 10px auto 0 auto;
+  width: 80%;
+  height: 40px;
+  border: none;
+  background-color: #1995f6;
+  color: white;
+  font-size: 16px;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:active {
+    background-color: #b2dffc;
+  }
+
+  &:disabled {
+    width: 80%;
+    margin: auto;
+    height: 40px;
+    margin-bottom: 80px;
+    margin-top: 10px;
+    border: none;
+    background-color: #b2dffc;
+    color: white;
+    font-size: 16px;
+    border-radius: 5px;
+    cursor: default;
+  }
+`;
