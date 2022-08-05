64a627736b768ddc89928c382502c261bb74fa4a
parkjooyoung <mirkduddl@naver.com>
2022-08-05 21:34:41 +0900

    Move : Login, SignUp 컴포넌트 Auth 파일로 이동

--- wanted-pre-onboarding-challenge-fe-1-client/src/Router.js
+++ wanted-pre-onboarding-challenge-fe-1-client/src/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SignUp";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
 import TodoList from "./pages/TodoList/TodoList";
 
 const Router = () => {
