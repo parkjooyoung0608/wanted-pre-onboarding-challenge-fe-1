986115e2a95811602b127968c918509bd2bb201f
parkjooyoung <mirkduddl@naver.com>
2022-08-07 15:56:41 +0900

    Add : TodoCreate Component 생성 및 UI 완료

--- wanted-pre-onboarding-challenge-fe-1-client/src/pages/TodoList/TodoList.js
+++ wanted-pre-onboarding-challenge-fe-1-client/src/pages/TodoList/TodoList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TodoTemplate from "./TodoTemplate";
 import TodoListBlock from "./TodoListBlock";
+import TodoCreate from "./TodoCreate";
 
 const TodoList = () => {
   return (
     <TodoTemplate>
       <TodoListBlock />
+      <TodoCreate />
     </TodoTemplate>
   );
 };
