e6717abdee6bcd4cebe6577c1982a2bea58ea4fe
parkjooyoung <mirkduddl@naver.com>
2022-08-07 17:40:07 +0900

    Add : TodoCreate 컴포넌트 추가 버튼 생성

--- wanted-pre-onboarding-challenge-fe-1-client/src/pages/TodoList/TodoCreate.js
+++ wanted-pre-onboarding-challenge-fe-1-client/src/pages/TodoList/TodoCreate.js
@@ -34,10 +34,11 @@ const TodoCreate = () => {
           <InsertForm onSubmit={onSubmit}>
             <Input
               autoFocus
-              placeholder="할 일을 입력 후, Enter 를 누르세요"
+              placeholder="할 일을 입력 후, Enter 또는 추가버튼을 누르세요"
               onChange={onChange}
               value={value}
             />
+            <InputButton>추가</InputButton>
           </InsertForm>
         </InsertFormPositioner>
       )}
@@ -103,22 +104,30 @@ const InsertFormPositioner = styled.div`
 
 const InsertForm = styled.form`
   background: #f8f9fa;
-  padding-left: 32px;
-  padding-top: 32px;
-  padding-right: 32px;
-  padding-bottom: 72px;
-
+  padding: 32px 32px 72px 32px;
   border-bottom-left-radius: 16px;
   border-bottom-right-radius: 16px;
   border-top: 1px solid #e9ecef;
 `;
 
 const Input = styled.input`
+  margin-right: 10px;
   padding: 12px;
+  width: 80%;
+  font-size: 18px;
   border-radius: 4px;
   border: 1px solid #dee2e6;
-  width: 100%;
   outline: none;
+`;
+
+const InputButton = styled.button`
+  padding: 11.5px;
+  width: calc(20% - 10px);
   font-size: 18px;
-  box-sizing: border-box;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+
+  &:hover {
+    background-color: #63e6be;
+  }
 `;
